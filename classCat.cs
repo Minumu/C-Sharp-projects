73a4dac837a4aa752310dab9c1820bba2c7035d6
Tania Shevchuk <taniaeru23@gmail.com>
2018-03-11 22:19:22 +0200

    created simple classes

---
+++ classCat.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace classCat
+{
+    public enum Gender { Male, Female };
+
+    public class Cat
+    {
+        public string Name { get; }
+        public Gender Gender { get; }
+        private double _energy;
+        public static readonly double MaxEnergy = 20;
+        public static readonly double MinEnergy = 0;
+        public static readonly double SleepEnergyGain = 10;
+        public static readonly double JumpEnergyDrain = 0.5;
+        public double Energy
+        {
+            get { return _energy; }
+            private set
+            {
+                if (value < MinEnergy)
+                    throw new Exception("Not enough energy to jump");
+                if (value > MaxEnergy)
+                    throw new Exception("Full energy");
+                else
+                    _energy = value;
+            }
+        }
+        public Cat(string name, Gender gender)
+        {
+            Name = name;
+            Gender = gender;
+            Energy = MaxEnergy;
+        }
+        public void Jump()
+        {
+            Energy -= JumpEnergyDrain; 
+        }
+        public void Sleep()
+        {
+            Energy += SleepEnergyGain;
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Cat Bob = new Cat("Bob", Gender.Male);
+            try
+            {
+                while (Bob.Energy >= 0)
+                {
+                    Console.WriteLine(Bob.Name + ", " + Bob.Gender + ", " + Bob.Energy);
+                    Bob.Jump();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ classDuck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace classes
+{
+    class Duck
+    {
+        public Duck(string name, string color, int life)
+        {
+            Name = name;
+            Color = color;
+            Life = life;
+        }
+        public string Color;
+        public string Name;
+        public int Life;
+
+        public void Fly(string place)
+        {
+            Console.WriteLine("Flying... " + place + ". Duck name: " + Name + ", duck color: " + Color + ", life is: " + Life);
+        }
+    }
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Duck johnDuck = new Duck("John", "Black", 2);
+            johnDuck.Fly("tree");
+            Duck bobDuck = new Duck("Bob", "Blue", 1);
+            bobDuck.Color = "white";
+            bobDuck.Fly("sky");
+        }
+    }
+}
